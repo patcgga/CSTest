71b182e378f50a49a618b6f5db7993587b5ae3aa
patcgga <144556803+patcgga@users.noreply.github.com>
2023-10-18 01:38:07 +0800

    CSTest

--- IF陳述式練習/7.if和else應用2/Program.cs
+++ IF陳述式練習/7.if和else應用2/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("TEST");
+﻿Random dice = new Random();
+int roll1 = dice.Next(1,7);
+int roll2 = dice.Next(1,7);
+int roll3 = dice.Next(1,7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rollled double +2 bonus to total!");
+        total += 2;
+    }
+}
+if (total >=15)
+{
+    Console.WriteLine("You win");
+}
+else
+{
+    Console.WriteLine("Sorry,you lose.");
+}
